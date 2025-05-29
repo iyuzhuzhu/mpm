f7343b747d749b67328e79066f886a534e88a596
iyuzhuzhu <2044551518@qq.com>
2025-05-29 17:04:12 +0800

    读取Isdisrup

--- mpm/test3.py
+++ mpm/test3.py
@@ -1,28 +1,22 @@
 # %%
 from jddb.meta_db import MetaDB
 
-
-
 # %% connect to the MetaDB
 connection_str = {
-    "host": 'www.limfx.pro',
+    "host": "www.limfx.pro",
     "port": 24007,
-    "database": "JDDB"
+    "database": "DDB",
+    "username": "DDBUser",
+    "password": "tokamak!"
 }
-#
-# connection_str = {
-#     "host": "localhost",
-#     "port": 27017,
-#     "database": "JDDB"
-# }
 
-collection = "Labels"
+collection = "tags"
 
 db = MetaDB(connection_str, collection)
 
 # %%
 #  find all the shot with shot_no in range [10000, 20000] && [IP, BT] tags available && is disruption
-shot_list = [shot for shot in range(1103600, 1103900)]
+shot_list = [shot for shot in range(1054200, 1054300)]
 complete_disruption_shots = db.query_valid(
     shot_list=shot_list,
     label_true=["IsDisrupt", "ip", "bt"]
